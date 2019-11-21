95448a4e473a1400b7cb31741a42f62041c4a720
SiwekD <43846253+SiwekD@users.noreply.github.com>
2019-11-22 00:31:42 +0100

    hehe

--- dane_zamowienie.php
+++ dane_zamowienie.php
@@ -10,6 +10,50 @@
 	
 ?>
 
+<?php 
+
+	if ( isset( $_GET['id'] ) && !empty( $_GET['id'] ) )
+	{
+	$id_zamowienie = $_GET['id'];
+	} else {
+	$id_zamowienie = 0; }
+
+	$con = mysqli_connect("localhost","root","","user");
+	mysqli_query($con, "SET CHARSET utf8");
+	mysqli_query($con, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+	$query = "SELECT * FROM zamowienie_informacje WHERE id_zamowienie = '$id_zamowienie' ";
+	$result = mysqli_query($con,$query);
+	$zamowienie = array();
+	$uzytkownik = array();
+	$przedmioty = array();
+	$x = 0;
+	while ($r = $result->fetch_array(MYSQLI_ASSOC)) {
+		$id_zamowienie=$r['id_zamowienie'];
+		$zamowienie[] = $r;
+		$id_user=$r['id_user'];
+		$qu_u = "SELECT * FROM uzytkownicy WHERE id_user = '$id_user' ";
+		$re_u = mysqli_query($con,$qu_u);
+			while ($rr_u = $re_u->fetch_array(MYSQLI_ASSOC)) {
+			$uzytkownik[] = $rr_u;
+		}
+		$qu = "SELECT * FROM zamowienie_przedmiot WHERE id_zamowienie = '$id_zamowienie' ";
+		$re = mysqli_query($con,$qu);
+		while ($rr = $re->fetch_array(MYSQLI_ASSOC)) {
+		$przedmioty[$x][0] = $rr;
+		$przed = mysqli_connect("localhost","root","","przedmioty");
+		mysqli_query($przed, "SET CHARSET utf8");
+		mysqli_query($przed, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");	
+		$id_produktu = $rr['id_produktu'];
+		$q = "SELECT * FROM przedmioty_ogolne_informacje WHERE id_produktu = '$id_produktu' ";
+		$res = mysqli_query($przed,$q);
+		while ($rrr = $res->fetch_array(MYSQLI_ASSOC)) {
+			$przedmioty[$x][1] = $rrr;
+		}
+		$x++;
+	}
+}
+?>
+
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
@@ -65,27 +109,35 @@
 			<div id="main_content">
 				<div id="panel_admin_border">
 					<div id="panel_admin">
-						Zamówienie **/****
+						Zamówienie nr <?php echo "".$id_zamowienie."";?>
 					</div>
 				</div>
 				<div class="half_width">
 					<span class="info_span">Dane podstawowe</span>
 					<div class="bordered_div_no_padding">
-						<div class="row"><div class="half_row">Data zamówienia:</div>				<div class="half_row_right"><span id="data">Data</span></div></div>
+					<?php 
+					$data_z = "".$zamowienie[0]['data_zamowienia']."";
+					$data = \DateTime::createFromFormat('D M d Y H:i:s e+', $data_z);
+					$data = $data->format('d/m/Y');
+					$dostawa  = "".$zamowienie[0]['dostawa']."";
+					$dostawa_ = explode(",", $dostawa);
+					if($dostawa_[1] == '') {$dostawa_[1] = "0";};
+					?>
+						<div class="row"><div class="half_row">Data zamówienia:</div>				<div class="half_row_right"><span id="data"><?php echo "".$data."";?></span></div></div>
 						
-						<div class="row"><div class="half_row">Wartość zamówienia:</div>			<div class="half_row_right"><span id="wartosc">Wartość</span></div></div>
+						<div class="row"><div class="half_row">Wartość zamówienia:</div>			<div class="half_row_right"><span id="wartosc"><?php echo "".$zamowienie[0]['cena_zamowienia']."";?> zł</span></div></div>
 						
-						<div class="row"><div class="half_row">Sposób płatności:</div>				<div class="half_row_right"><select class="tx" id="platnosc"></select></div></div>
+						<div class="row"><div class="half_row">Sposób płatności:</div>				<div class="half_row_right"><span id="platnosc"><?php echo "".$zamowienie[0]['platnosc']."";?></span></div></div>
 						
-						<div class="row"><div class="half_row">Sposób dostawy:</div>				<div class="half_row_right"><select class="tx" id="dostawa"></select></div></div>
+						<div class="row"><div class="half_row">Sposób dostawy:</div>				<div class="half_row_right"><span id="dostawa"><?php echo "".$dostawa_[0]."";?></span></div></div>
 						
-						<div class="row"><div class="half_row">Koszt dostawy:</div>					<div class="half_row_right"><span id="koszt_dostawy">Koszt dostawy</span></div></div>
+						<div class="row"><div class="half_row">Koszt dostawy:</div>					<div class="half_row_right"><span id="koszt_dostawy"><?php echo "".$dostawa_[1]."";?> zł</span></div></div>
 						
-						<div class="row"><div class="half_row">Aktualny status zamówienia:</div>	<div class="half_row_right"><span id="status">Status</span></div></div>
+						<div class="row"><div class="half_row">Aktualny status zamówienia:</div>	<div class="half_row_right"><span id="status"><?php echo "".$zamowienie[0]['status']."";?></span></div></div>
 						
-						<div class="row"><div class="half_row">Opiekun zamówienia:</div>		<div class="half_row_right"><select class="tx" id="opiekun"></select></div></div>
+						<div class="row"><div class="half_row">Opiekun zamówienia:</div>		<div class="half_row_right"><select class="tx" id="opiekun"><option> Dawid </option></select></div></div>
 						
-						<div class="row"><div class="half_row">Termin dostawy:</div>			<div class="half_row_right"><input class="tx" type="text" name="termin"/></div></div>
+						<div class="row"><div class="half_row">Termin dostawy:</div>			<div class="half_row_right"><input class="tx" type="text" name="termin" value="<?php echo "".$zamowienie[0]['szacowana_data_dostawy']."";?>"/></div></div>
 						
 						<div class="row"><div class="half_row">Numer paczki:</div>				<div class="half_row_right"><input class="tx" type="text" name="nr_paczki"/></div></div>
 						
@@ -96,33 +148,55 @@
 				<div class="half_width">
 					<span class="info_span">Dane klienta</span>
 					<div class="bordered_div_no_padding">
-						<div class="row"><div class="half_row">Klient:</div>								<div class="half_row_right"><span id="klient">Klient_nick</span></div></div>
+						<div class="row"><div class="half_row">Klient:</div>								<div class="half_row_right"><span id="klient"><b>Nick: </b><?php echo "".$uzytkownik[0]['nick'].",  <b>Imię i nazwisko: </b>".$uzytkownik[0]['name']." ".$uzytkownik[0]['surname']."";?></span></div></div>
+						<?php 
+						$adres  = "".$zamowienie[0]['adres']."";
+						$adres_ = explode(",", $adres);
+						?>
+						<div class="row" style="border:0px"><div class="half_row">Dane do dostawy:</div>	<div class="half_row_right"><input class="tx" type="text" name="imienazwisko" value="<?php echo "".$adres_[0]." ".$adres_[1]."";?>"/></div></div>
 						
-						<div class="row" style="border:0px"><div class="half_row">Dane do dostawy:</div>	<div class="half_row_right"><input class="tx" type="text" name="ulica"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row"></div>					<div class="half_row_right"><input class="tx" type="text" name="telefon" value="<?php echo "".$adres_[6]."";?>"/></div></div>
 						
-						<div class="row" style="border:0px"><div class="half_row"></div>					<div class="half_row_right"><input class="tx" type="text" name="nr_domu"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row"></div>					<div class="half_row_right"><input class="tx" type="text" name="adres" value="<?php echo "".$adres_[3]."";?>"/></div></div>
 						
-						<div class="row"><div class="half_row"></div>										<div class="half_row_right"><input class="tx" style="width:29%; margin-right:1%;" type="text" name="kod_pocztowy"/><input class="tx" style="width:70%" type="text" name="miejscowosc"/></div></div>
+						<div class="row"><div class="half_row"></div>										<div class="half_row_right"><input class="tx" style="width:29%; margin-right:1%;" type="text" name="kod_pocztowy" value="<?php echo "".$adres_[4]."";?>"/><input class="tx" style="width:70%" type="text" name="miejscowosc" value="<?php echo "".$adres_[5]."";?>"/></div></div>
 						
-						<div class="row" style="border:0px"><div class="half_row">Dane do faktury:</div>	<div class="half_row_right"><input class="tx" type="text" name="faktura_ulica"/></div></div>
+						<?php 
+						$faktura  = "".$zamowienie[0]['faktura']."";
+						if($faktura != '') {$faktura_ = explode(",", $faktura); $fa = true;} else {$faktura_ = array(); $fa=false;};
+						$dane  = "".$zamowienie[0]['dane_odbiorcy']."";
+						$dane_ = explode(",", $dane);
+						?>
 						
-						<div class="row" style="border:0px"><div class="half_row"></div>					<div class="half_row_right"><input class="tx" type="text" name="faktura_nr_domu"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row">Dane do faktury:</div>	<div class="half_row_right"><input class="tx" type="text" name="faktura_imie" value="<?php echo "".$faktura_[0]." ".$faktura_[1]."";?>" /></div></div>
 						
-						<div class="row" style="border:0px"><div class="half_row"></div>										<div class="half_row_right"><input class="tx" style="width:29%; margin-right:1%;" type="text" name="faktura_kod_pocztowy"/><input class="tx" style="width:70%" type="text" name="faktura_miejscowosc"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row"></div>					<div class="half_row_right"><input class="tx" type="text" name="faktura_nazwa_adres" value="<?php echo "".$faktura_[2]."";?>"/></div></div>
 						
-						<div class="row"><div class="half_row"></div>										<div class="half_row_right">NIP:<input class="tx" style="width:70%; margin-left:5px;" type="text" name="faktura_nip"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row"></div>										<div class="half_row_right"><input class="tx" style="width:29%; margin-right:1%;" type="text" name="faktura_kod_pocztowy" value="<?php echo "".$faktura_[3]."";?>"/><input class="tx" style="width:70%" type="text" name="faktura_miejscowosc" value="<?php echo "".$faktura_[4]."";?>"/></div></div>
 						
-						<div class="row" style="border:0px"><div class="half_row">Kontakt:</div>			<div class="half_row_right kontakt"><div class="kontakt_h_l">Telefon:</div><input class="tx" style="width:60%;" type="text" name="telefon"/></div></div>
+						<div class="row"><div class="half_row"></div>										<div class="half_row_right">NIP:<input class="tx" style="width:70%; margin-left:5px;" type="text" name="faktura_nip" value="<?php echo "".$adres_[2]."";?>"/></div></div>
 						
-						<div class="row"><div class="half_row"></div>										<div class="half_row_right kontakt"><div class="kontakt_h_l">E-mail:</div><input class="tx" style="width:60%;" type="text" name="email"/></div></div>
+						<div class="row" style="border:0px"><div class="half_row">Kontakt:</div>			<div class="half_row_right kontakt"><div class="kontakt_h_l">Telefon:</div><input class="tx" style="width:60%;" type="text" name="telefon" value="<?php echo "".$dane_[2]."";?>"/></div></div>
 						
-						<div class="row"><div class="half_row">Wybrany dokument:</div>						<div class="half_row_right"><input type="radio" value="paragon" name="dokument">paragon <input type="radio" value="faktura" name="dokument" checked>faktura </div></div>
+						<div class="row"><div class="half_row"></div>										<div class="half_row_right kontakt"><div class="kontakt_h_l">E-mail:</div><input class="tx" style="width:60%;" type="text" name="email"  value="<?php echo "".$dane_[3]."";?>"/></div></div>
+						
+						<div class="row"><div class="half_row">Wybrany dokument:</div>						<div class="half_row_right"><input type="radio" value="paragon" name="dokument" <?php if($fa == false) {echo "checked";}?>>paragon <input type="radio" value="faktura" name="dokument" <?php if($fa == true) {echo "checked";}?>>faktura </div></div>
 						
 						<div class="row"><div class="half_row">Zapłacono:</div>								<div class="half_row_right"><input class="tx" style="width:30%;" type="number" name="zaplacono"/></div></div>
+						<?php 
+						$rabat =  "".$zamowienie[0]['rabat']."";
+						if($rabat != '') {
+							$rrab = true;
+							$quer = "SELECT * FROM kod_rabatowy WHERE kod='$rabat' ";
+							$resu = mysqli_query($con,$quer);
+							while ($r = $resu->fetch_array(MYSQLI_ASSOC)) {
+								$procent = $r['rabat'];
+							}
+						} else {$procent = "Brak rabatu"; $rrab = false;};
+						?>
+						<div class="row"><div class="half_row">Naliczony rabat [%]:</div>						<div class="half_row_right"><span id="rabat"><?php echo "".$procent."";?></span></div></div>
 						
-						<div class="row"><div class="half_row">Naliczony rabat:</div>						<div class="half_row_right"><span id="rabat">[rabat]</span></div></div>
-						
-						<div class="row"><div class="half_row">Komentarz klienta:</div>						<div class="half_row_right"><span id="komentarz_klienta">[komentarz klienta]</span></div></div>
+						<div class="row"><div class="half_row">Komentarz klienta:</div>						<div class="half_row_right"><span id="komentarz_klienta"><?php echo "".$zamowienie[0]['info_sprzedawca']."";?></span></div></div>
 						
 						<div class="row"><div class="half_row">Komentarz wysyłany do klienta:</div>			<div class="half_row_right"><textarea class="areatx tx" rows="4"></textarea></div></div>
 						
@@ -137,20 +211,43 @@
 							<div class="row">
 								<div class="hr1">LP</div>	<div class="hr2">ZDJĘCIE</div> 	<div class="hr3">NAZWA</div>	<div class="hr4">ILOŚĆ</div>	<div class="hr5">WARTOŚĆ</div>
 							</div>
+							<?php 
+							$h=0;
+							$suma_p = 0;
+							for($t=0;$t<count($przedmioty);$t++)
+							{
+								$localization = "".$przedmioty[$t][1]['lokalizacja']."";
+								$src = "".$przedmioty[$t][1]['zdjecie']."";
+								$nazwa = "".$przedmioty[$t][1]['pelna_nazwa']."";
+								$ilosc = "".$przedmioty[$t][0]['quantity']."";
+								$cena = "".$przedmioty[$t][0]['price_one_quan']."";
+								$sum = (int)$ilosc * (int)$cena;
+								$suma_p = $suma_p + $sum;
+							echo "<div class='row'>
+								<div class='hr1'>".$h."</div>	<div class='hr2'><img src='../lepsza/category/".$localization."/".$src."'/></div> 	<div class='hr3'>".$nazwa."</div>	<div class='hr4'><span >".$ilosc."</span></div>	<div class='hr5'><span >".$sum." zł</span></div>
+							</div>";
+							$h++;
+							}
+							?>
 							<div class="row">
-								<div class="hr1">1</div>	<div class="hr2"><img src="./img/oops.png"/></div> 	<div class="hr3">Gostek który strzela się w łeb ! HIT ! PROC ĆWIERĆ RDZENIA !</div>	<div class="hr4"><input class="tx" style="width:100%;" type="number" name="cena1"/></div>	<div class="hr5"><span id="wartosc1">100zł</span></div>
+								<div class="hr2">Wartość produktów:				</div><div class="hr3"><span id="wartosc"><?php echo "".$suma_p.""?> zł</span></div>
 							</div>
 							<div class="row">
-								<div class="hr1">2</div>	<div class="hr2"><img src="./img/oops.png"/></div> 	<div class="hr3">Gostek który strzela się w łeb ! HIT ! PROC PÓŁ RDZENIA !</div>	<div class="hr4"><input class="tx" style="width:100%;" type="number" name="cena2"/></div>	<div class="hr5"><span id="wartosc2">1000zł</span></div>
+								<div class="hr2">Koszt dostawy:				</div><div class="hr3"><span id="koszt_dostawy"><?php echo "".$dostawa_[1]."";?> zł</span></div>
+							</div>
+							<?php 
+							$suma = (int)$dostawa + $suma_p;
+							if($rrab == true) {
+								$suma_rabat = ((int)$dostawa + $suma) * (int)$procent;
+								echo"<div class='row'>
+								<div class='hr2'>Rabat				</div><div class='hr3'><span id='rabat'>".$suma_rabat." zł</span></div>
+							</div>";}
+							?>
+							<div class="row">
+								<div class="hr2">VAT:				</div><div class="hr3"><span id="VAT"><?php echo "".$zamowienie[0]['vat23']."";?> zł</span></div>
 							</div>
 							<div class="row">
-								<div class="hr2">Wartość produktów:				</div><div class="hr3"><span id="wartosc">40000zł</span></div>
-							</div>
-							<div class="row">
-								<div class="hr2">Koszt dostawy:				</div><div class="hr3"><span id="koszt_dostawy">40zł</span></div>
-							</div>
-							<div class="row">
-								<div class="hr2">Razem:				</div><div class="hr3"><span id="do_zaplaty">40040zł</span></div>
+								<div class="hr2">Razem:				</div><div class="hr3"><span id="do_zaplaty"><?php echo "".$suma."";?> zł</span></div>
 							</div>
 						</div>
 					</div>
