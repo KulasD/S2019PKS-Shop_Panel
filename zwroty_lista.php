3a94f048ac363a297cf1121ff518f516f32920f1
SiwekD <43846253+SiwekD@users.noreply.github.com>
2019-12-06 16:00:00 +0100

    podpiecie, 5h

--- zwroty_lista.php
+++ zwroty_lista.php
@@ -10,6 +10,70 @@
 	
 ?>
 
+<?php
+	$con = mysqli_connect("localhost","root","","user");
+	mysqli_query($con, "SET CHARSET utf8");
+	mysqli_query($con, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+	$rows = array();
+	$rows_info = array();
+	$ids = array();
+	$user = array();
+	$query = "SELECT * FROM zwroty  ORDER BY id_zwrot ASC";
+	$result = mysqli_query($con,$query);
+	while ($r = $result->fetch_array(MYSQLI_ASSOC)) {
+		$rows[] = $r;
+		$ids[] = explode("|", $r['produkty']);
+		$id_user = $r['id_user'];
+		$query_u = "SELECT * FROM uzytkownicy WHERE id_user='$id_user'";
+		$result_u = mysqli_query($con,$query_u);
+		while($reu = $result_u->fetch_array(MYSQLI_ASSOC)) {
+			$user[] = $reu;
+		};	
+	}	
+	$j = -1;
+	for($i = 0; $i < count($ids) ; $i++)
+		{
+			for($k = 0; $k < count($ids[$i]); $k++)
+			{
+				$j++;
+				$id = explode(",", $ids[$i][$k]);
+				if($id[0] == '')
+				{} else {
+					$query_x = "SELECT * FROM zamowienie_przedmiot WHERE id_zamow_p='$id[0]'";
+					$result_x = mysqli_query($con,$query_x);
+					while($re = $result_x->fetch_array(MYSQLI_ASSOC)) {
+						$rows_product[$j] = $re;
+						$rows_product[$j]['ilosc']	= $id[1];
+						$id_produktu = $re['id_produktu'];
+						$pol = mysqli_connect("localhost","root","","przedmioty");
+						mysqli_query($pol, "SET CHARSET utf8");
+						mysqli_query($pol, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+						$getvalue="SELECT * FROM przedmioty_ogolne_informacje WHERE id_produktu='$id_produktu'";
+						$rezul=mysqli_query($pol,$getvalue);
+						while ($q = $rezul->fetch_array(MYSQLI_ASSOC)) {
+							$rows_info[$j] = $q;				
+							}						
+						}	
+					}
+			}
+			$rows_product[$j] = "pause";
+			$rows_info[$j] = "pause";
+		};
+	$order_all = array();
+	$hel = 0;
+	$ind = 0;
+	for($r=0;$r<count($rows_info);$r++)
+		{
+			if($rows_info[$r] == "pause")
+			{$hel++; $ind=0;} else {
+				$order_all[$hel][$ind]=$rows_info[$r];
+				$order_all[$hel][$ind]["info"] =$rows_product[$r];
+				$ind++;
+			}
+	};
+	
+?>
+
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
@@ -72,7 +136,6 @@
 						<div class="bordered_div_no_padding">
 							<div class="row">
 								<div class="hr_k1">
-									<span class="one_line_span">ID</span>
 									<span class="one_line_span">STATUS</span>
 									<span class="one_line_span">ID ZWROTU</span>
 								</div>	
@@ -85,79 +148,55 @@
 								<div class="hr_k4">DATA ZGŁOSZENIA</div>	
 								<div class="hr_k5">DZIAŁANIA</div>
 							</div>
-							<div class="row">
-								<div class="hr_k1">
-									<span class="one_line_span">1</span>
-									<span class="one_line_span">status</span>
-									<span class="one_line_span click_me_span">20/20020</span>
-								</div>	
-								<div class="hr_k2">
-									<span class="one_line_span click_me_span">Teodory Kopra</span>
-									<span class="one_line_span click_me_span">tajny_email@firma.com</span>
-								</div> 	
-								<div class="hr_k3">
-									<div class="flex_box">
-										<div class="f_z">
-											<img src="./img/oops.png"/>
+						<?php
+							for($d=0;$d<count($rows);$d++)
+							{
+								echo "<div class='row'>
+									<div class='hr_k1'>
+										<span class=one_line_span>".$rows[$d]['status']."</span>
+										<span class='one_line_span '>".$rows[$d]['id_zwrot']."</span>
+									</div>	
+									<div class='hr_k2'>
+										<span class='one_line_span '>".$user[$d]['name']." ".$user[$d]['surname']."</span>
+										<span class='one_line_span'>".$user[$d]['email']."</span>
+									</div><div class='hr_k3'>";
+									for($s=0;$s<count($order_all[$d]);$s++)
+									{
+										$localization = "".$order_all[$d][$s]['lokalizacja']."";
+										$src = "".$order_all[$d][$s]['zdjecie']."";
+										echo "
+										<div class='flex_box'>
+											<div class='f_z'>
+												<img src='../lepsza/category/".$localization."/".$src."'/>
+											</div>
+											<div id='produkt_1' class='n_z'>
+												".$order_all[$d][$s]['pelna_nazwa']." <br />
+												<b>Sztuk: ".$order_all[$d][$s]['info']['ilosc']."</b>
+											</div>
 										</div>
-										<div id="produkt_1" class="n_z">
-											Gostek który strzela się w łeb ! HIT ! PROC PÓŁ RDZENIA !
-										</div>
+										";
+									};
+									echo "</div>
+									<div class='hr_k4'>".$rows[$d]['data_zwrotu']."</div>	
+									<div class='hr_k5'>
+										<div class='s_d_b'><button type='button' class='button' onclick='zwrot_go(".$rows[$d]['id_zwrot'].")'>EDYTUJ</button></div>
+										<div class='s_d_b'><button type='button' class='red_button'>USUŃ</button></div>
 									</div>
-									<div class="flex_box">
-										<div class="f_z">
-											<img src="./img/oops.png"/>
-										</div>
-										<div id="produkt_1" class="n_z">
-											Gostek który strzela się w łeb ! HIT ! PROC PÓŁ RDZENIA !
-										</div>
-									</div>
-								</div>	
-								<div class="hr_k4">20.20.20</div>	
-								<div class="hr_k5">
-									<div class="s_d_b"><button type="button" class="button">EDYTUJ</button></div>
-									<div class="s_d_b"><button type="button" class="red_button">USUŃ</button></div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="hr_k1">
-									<span class="one_line_span">1</span>
-									<span class="one_line_span">status</span>
-									<span class="one_line_span click_me_span">20/20020</span>
-								</div>	
-								<div class="hr_k2">
-									<span class="one_line_span click_me_span">Teodory Kopra</span>
-									<span class="one_line_span click_me_span">tajny_email@firma.com</span>
-								</div> 	
-								<div class="hr_k3">
-									<div class="flex_box">
-										<div class="f_z">
-											<img src="./img/oops.png"/>
-										</div>
-										<div id="produkt_1" class="n_z">
-											Gostek który strzela się w łeb ! HIT ! PROC PÓŁ RDZENIA !
-										</div>
-									</div>
-									<div class="flex_box">
-										<div class="f_z">
-											<img src="./img/oops.png"/>
-										</div>
-										<div id="produkt_1" class="n_z">
-											Gostek który strzela się w łeb ! HIT ! PROC PÓŁ RDZENIA !
-										</div>
-									</div>
-								</div>	
-								<div class="hr_k4">20.20.20</div>	
-								<div class="hr_k5">
-									<div class="s_d_b"><button type="button" class="button">EDYTUJ</button></div>
-									<div class="s_d_b"><button type="button" class="red_button">USUŃ</button></div>
-								</div>
-							</div>
+								</div>";
+							};				
+						?>
 						</div>
 					</div>
 			</div>
 		</div>
 		<div style="clear:both;"></div>
 	</div>
+<script src="http://code.jquery.com/jquery-1.12.4.js"></script>
+<script type="text/javascript">
+function zwrot_go(nr)
+{
+	window.location.href = "dane_zwrot.php?id="+nr; 
+}
+</script>
 </body>
 </html>
