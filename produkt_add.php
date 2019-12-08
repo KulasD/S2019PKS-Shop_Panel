2d033e5fc7c0e6b2f6bc04aa713897c87f7d69a5
SiwekD <43846253+SiwekD@users.noreply.github.com>
2019-12-08 15:28:20 +0100

    Co poprawiłem masz na mesenger

--- produkt_add.php
+++ produkt_add.php
@@ -30,11 +30,11 @@
 	//echo $cena;
 	$sztuki = $_POST['sztuki'];
 	//echo $sztuki;
-	$parametr1 = $_POST['parametr1'];
+	if(isset($_POST['parametr1'])) {$parametr1 = $_POST['parametr1']; } else {$parametr1 = '';};
 	//echo $parametr1;
-	$parametr2 = $_POST['parametr2'];
+	if(isset($_POST['parametr2'])) {$parametr2 = $_POST['parametr2']; } else {$parametr2 = '';};
 	//echo $parametr2;
-	$parametr3 = $_POST['parametr3'];
+	if(isset($_POST['parametr3'])) {$parametr3 = $_POST['parametr3']; } else {$parametr3 = '';};
 	//echo $parametr3;
 	$kategoria = $_SESSION['kat'];
 	//echo $kategoria;
@@ -63,7 +63,8 @@
 	mysqli_query($con,$sql);
 	
 	//------------------------------------------------------ OPIS_PRZEDMIOTU
-	
+	$n1 = ''; $o1 = ''; $n2 =''; $o2 = '';$n3 =''; $o3 = '';
+	$src1 = ''; $src2 = ''; $src3 = '';
 	$n1 = $_POST['n1'];
 	$o1 = $_POST['o1'];
 	$n2 = $_POST['n2'];
@@ -73,31 +74,38 @@
 	//echo $n1.$o1;
 	//echo $n2.$o2;
 	//echo $n3.$o3;
+	
+	if($n1 == '' || $o1 == '') {$z1 = false;} else {
+	$z1 = true;
 	$src1 = "img/".basename( $_FILES['zdj1']['name']);
 	$query_opis1 = ("INSERT INTO opis_przedmiotu VALUES (NULL, '$id','$n1', '$o1', '$src1')");
-	mysqli_query($con,$query_opis1);
+	mysqli_query($con,$query_opis1);}
 	
+	if($n2 == '' || $o2 == '') {$z2 = false;} else {
+	$z2 = true;
 	$src2 = "img/".basename( $_FILES['zdj2']['name']);
 	$query_opis2 = ("INSERT INTO opis_przedmiotu VALUES (NULL, '$id','$n2', '$o2', '$src2')");
-	mysqli_query($con,$query_opis2);
+	mysqli_query($con,$query_opis2); }
 	
+	if($n3 == '' || $o3 == '') {$z3 = false;} else {
+	$z3 = true;
 	$src3 = "img/".basename( $_FILES['zdj3']['name']);
 	$query_opis3 = ("INSERT INTO opis_przedmiotu VALUES (NULL, '$id','$n3', '$o3', '$src3')");
-	mysqli_query($con,$query_opis3);
+	mysqli_query($con,$query_opis3); }
 	
 	//------------------------------------------------------ ZDJECIA_PRZEDMIOTOW
 	
 	$query_zdjecie1 = ("INSERT INTO zdjecia_przedmiotow VALUES (NULL, '$id', '$src')");
 	mysqli_query($con,$query_zdjecie1);
-	
+	if($src1 == '') {} else {
 	$query_zdjecie2 = ("INSERT INTO zdjecia_przedmiotow VALUES (NULL, '$id', '$src1')");
-	mysqli_query($con,$query_zdjecie2);
-	
+	mysqli_query($con,$query_zdjecie2);}
+	if($src2 == '') {} else {
 	$query_zdjecie3 = ("INSERT INTO zdjecia_przedmiotow VALUES (NULL, '$id', '$src2')");
-	mysqli_query($con,$query_zdjecie3);
-	
+	mysqli_query($con,$query_zdjecie3);}
+	if($src3 == '') {} else {
 	$query_zdjecie4 = ("INSERT INTO zdjecia_przedmiotow VALUES (NULL, '$id', '$src3')");
-	mysqli_query($con,$query_zdjecie4);
+	mysqli_query($con,$query_zdjecie4);}
 	
 	//------------------------------------------------------ OBSLUGA DODAWANIA ZDJEC
 	//------------------------------------------------------ PEWNIE SIE TO DA ZROBIC W FUNKCJI JAKIEJS ALE DODAJE SIE I TAK TYLKO 4 ZDJECIA MAX WIEC NIE PRZERABIAM
@@ -138,7 +146,7 @@
 	}
 	
 	//------------------------------------------------------ ZDJECIE DODATKOWE 1
-	
+	if($z1 == true) {
 	$target_file = $target_dir . basename($_FILES['zdj1']['name']);
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -171,9 +179,10 @@
 			echo "Sorry, there was an error uploading your file.";
 		}
 	}
+	}
 	
 	//------------------------------------------------------ ZDJECIE DODATKOWE 2
-	
+	if($z2 == true) {
 	$target_file = $target_dir . basename($_FILES['zdj2']['name']);
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -206,42 +215,40 @@
 			echo "Sorry, there was an error uploading your file.";
 		}
 	}
-	
+	}
 	//------------------------------------------------------ ZDJECIE DODATKOWE 3
-	
-	$target_file = $target_dir . basename($_FILES['zdj3']['name']);
-	$uploadOk = 1;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	// Sprawdzenie czy to jest image
-	$check = getimagesize($_FILES['zdj3']['tmp_name']);
-	if($check !== false) {
-		echo "File is an image - " . $check['mime'] . ".";
+	if($z3 == true) {
+		$target_file = $target_dir . basename($_FILES['zdj3']['name']);
 		$uploadOk = 1;
-	} else {
-		echo "File is not an image.";
-		$uploadOk = 0;
-	}
-	// sprawdz czy plik ma format JPG, JPEG, PNG, GIF
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"&& $imageFileType != "gif" ) {
-		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		$uploadOk = 0;
-	}
-	//sprawdz czy juz jest taka fotka
-	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	//jesli nie ma bledow to wysyla zdjecie
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.";
-	} else {
-		if (move_uploaded_file($_FILES['zdj3']['tmp_name'], $target_file)) {
-			echo "The file ". basename( $_FILES['zdj3']['name']). " has been uploaded.";
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+		// Sprawdzenie czy to jest image
+		$check = getimagesize($_FILES['zdj3']['tmp_name']);
+		if($check !== false) {
+			echo "File is an image - " . $check['mime'] . ".";
+			$uploadOk = 1;
 		} else {
-			echo "Sorry, there was an error uploading your file.";
+			echo "File is not an image.";
+			$uploadOk = 0;
+		}
+		// sprawdz czy plik ma format JPG, JPEG, PNG, GIF
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"&& $imageFileType != "gif" ) {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+			$uploadOk = 0;
+		}
+		//sprawdz czy juz jest taka fotka
+		if (file_exists($target_file)) {
+			echo "Sorry, file already exists.";
+			$uploadOk = 0;
+		}
+		//jesli nie ma bledow to wysyla zdjecie
+		if ($uploadOk == 0) {
+			echo "Sorry, your file was not uploaded.";
+		} else {
+			if (move_uploaded_file($_FILES['zdj3']['tmp_name'], $target_file)) {
+				echo "The file ". basename( $_FILES['zdj3']['name']). " has been uploaded.";
+			} else {
+				echo "Sorry, there was an error uploading your file.";
+			}
 		}
 	}
-	
-
-
 ?>
